84a3b8a20ff475ee5de4f87978b97e51db2cdc8b
Rupa Sravani Kommineni <rupasravanikommineni@gmail.com>
2014-03-15 18:45:37 -0500

    Configuring style sheets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Final::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( application.css, books.css.scss, users.css.scss )
+  config.assets.precompile += %w( application.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
